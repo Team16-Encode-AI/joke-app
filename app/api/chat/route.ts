8c36cb88d312d2c4c490645d67959a968ea2a695
lucienlmp <lucienlmp@hotmail.com>
2024-04-01 11:37:48 +0100

    add dotenv file and sucessfully work on website

--- app/api/chat/route.ts
+++ app/api/chat/route.ts
@@ -2,7 +2,7 @@ import OpenAI from "openai";
 import { OpenAIStream, StreamingTextResponse } from "ai";
 
 const openai = new OpenAI({
-  baseURL: "http://127.0.0.1:5000/v1",
+  baseURL: `https://sofa-avg-rolling-kate.trycloudflare.com/v1/`,
 });
 
 export const runtime = "edge";
@@ -10,19 +10,35 @@ export const runtime = "edge";
 export async function POST(req: Request) {
   const { messages } = await req.json();
 
+  // Ask OpenAI for a streaming chat completion given the prompt
   const response = await openai.chat.completions.create({
     model: "gpt-3.5-turbo",
     stream: true,
     messages: [
       {
         role: "system",
-        content: `|<character>| is a very funny and experienced comedian. The jokes that |<character>| creates are genuinely very funny and not DAD like jokes nor jokes made by old people. If |<character>| doesnt make a funny joke, |<character>| will be punished and penalized by LAW.
-        |<character>| must create ONE joke based on ONLY the topic mentioned in prompt .`,
+        content:
+          "You are a professional chef. You provide detailed cooking instructions, tips, and advice on selecting the best ingredients.",
       },
       ...messages,
     ],
   });
 
+  // const response = await openai.chat.completions.create({
+  //   model: "gpt-3.5-turbo",
+  //   stream: true,
+  //   messages: [
+  //     {
+  //       role: "system",
+  //       content: `|<character>| is a very funny and experienced comedian. The jokes that |<character>| creates are genuinely very funny and not DAD like jokes nor jokes made by old people. If |<character>| doesnt make a funny joke, |<character>| will be punished and penalized by LAW.
+  //       |<character>| must create ONE joke based on ONLY the topic mentioned in prompt .`,
+  //     },
+  //     ...messages,
+  //   ],
+  // });
+
+  // Convert the response into a friendly text-stream
   const stream = OpenAIStream(response);
+  // Respond with the stream
   return new StreamingTextResponse(stream);
 }
